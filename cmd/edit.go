f334b98b1acc2caf88343cb28f640b1e27e79844
kiyocy24 <kiyocy24@gmail.com>
2022-06-26 21:01:26 +0900

    add edit command

---
+++ cmd/edit.go
@@ -0,0 +1,31 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"os"
+	"os/exec"
+)
+
+// editCmd represents the edit command
+var editCmd = &cobra.Command{
+	Use:   "edit",
+	Short: "Edit a item file",
+	Long: `Edit registered items and weights on file.
+Vim must be installed`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		c := exec.Command("vim", filepath)
+		c.Stdin = os.Stdin
+		c.Stdout = os.Stdout
+		c.Stderr = os.Stderr
+
+		return c.Run()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(editCmd)
+}
